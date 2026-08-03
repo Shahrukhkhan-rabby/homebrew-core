25e895214aab9223726482433d2fe8ceb07c9a0c
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2026-08-03 14:22:06 +0600

    Formula y/yt-dlp.rb

---
+++ Formula/y/yt-dlp.rb
@@ -0,0 +1,93 @@
+class YtDlp < Formula
+  include Language::Python::Virtualenv
+
+  desc "Feature-rich command-line audio/video downloader"
+  homepage "https://github.com/yt-dlp/yt-dlp"
+  url "https://files.pythonhosted.org/packages/8b/34/7c6b4e3f89cb6416d2cd7ab6dab141a1df97ab0fb22d15816db2c92148c9/yt_dlp-2026.3.17.tar.gz"
+  sha256 "ba7aa31d533f1ffccfe70e421596d7ca8ff0bf1398dc6bb658b7d9dec057d2c9"
+  license "Unlicense"
+  revision 2
+  compatibility_version 1
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a993e3e8cd567b9a16271c82ed9696d626016f79b133c6f18f777df984574649"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7a387812f49e4dcb83434422bfa5636233ea0d449e24715978c2925ed4f4548"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ed215fdebc5470d68be798292df2273c9c19c6ffc5e5ff990c01bd48e50d968d"
+    sha256 cellar: :any_skip_relocation, sonoma:        "0f94d1e0f294cceb99ec0896ebf63daff604af2133800dcb298e5ed9d436c33e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f8e5f98eeadb9780a368da25b29efc1a0bc8daf20490b275b0ce11dc7c163511"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6de3556c6c5a0e2c4d6ddc0eaf84d57ed164157025a68da80e297b63948095b"
+  end
+
+  head do
+    url "https://github.com/yt-dlp/yt-dlp.git", branch: "master"
+
+    depends_on "pandoc" => :build
+  end
+
+  depends_on "node" => :build # https://github.com/yt-dlp/ejs/issues/37
+  depends_on "certifi"
+  depends_on "deno"
+  depends_on "python@3.14"
+
+  pypi_packages package_name:     "yt-dlp[default]",
+                exclude_packages: "certifi"
+
+  resource "brotli" do
+    url "https://files.pythonhosted.org/packages/f7/16/c92ca344d646e71a43b8bb353f0a6490d7f6e06210f8554c8f874e454285/brotli-1.2.0.tar.gz"
+    sha256 "e310f77e41941c13340a95976fe66a8a95b01e783d430eeaf7a2f87e0a57dd0a"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/e7/a1/67fe25fac3c7642725500a3f6cfe5821ad557c3abb11c9d20d12c7008d3e/charset_normalizer-3.4.7.tar.gz"
+    sha256 "ae89db9e5f98a11a4bf50407d4363e7b09b31e55bc117b4f7d80aab97ba009e5"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/82/77/7b3966d0b9d1d31a36ddf1746926a11dface89a83409bf1483f0237aa758/idna-3.15.tar.gz"
+    sha256 "ca962446ea538f7092a95e057da437618e886f4d349216d2b1e294abfdb65fdc"
+  end
+
+  resource "mutagen" do
+    url "https://files.pythonhosted.org/packages/81/e6/64bc71b74eef4b68e61eb921dcf72dabd9e4ec4af1e11891bbd312ccbb77/mutagen-1.47.0.tar.gz"
+    sha256 "719fadef0a978c31b4cf3c956261b3c58b6948b32023078a2117b1de09f0fc99"
+  end
+
+  resource "pycryptodomex" do
+    url "https://files.pythonhosted.org/packages/c9/85/e24bf90972a30b0fcd16c73009add1d7d7cd9140c2498a68252028899e41/pycryptodomex-3.23.0.tar.gz"
+    sha256 "71909758f010c82bc99b0abf4ea12012c98962fbf0583c2164f8b84533c2e4da"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/24/36/7180e7f077c38108945dbbdf60fe04db681c3feb6e96419f8c6dc8723741/requests-2.34.1.tar.gz"
+    sha256 "0fc5669f2b69704449fe1552360bd2a73a54512dfd03e65529157f1513322beb"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
+    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+  end
+
+  resource "websockets" do
+    url "https://files.pythonhosted.org/packages/04/24/4b2031d72e840ce4c1ccb255f693b15c334757fc50023e4db9537080b8c4/websockets-16.0.tar.gz"
+    sha256 "5f6261a5e56e8d5c42a4497b364ea24d94d9563e8fbd44e78ac40879c60179b5"
+  end
+
+  resource "yt-dlp-ejs" do
+    url "https://files.pythonhosted.org/packages/d3/e6/cceb9530e8f4e5940f6f7822d90e9d94f1b85343329a16baaf47bbbb3de1/yt_dlp_ejs-0.8.0.tar.gz"
+    sha256 "d5fa1639f63b5c4af8d932495f60689d5370f1a095782c944f7f62a303eb104e"
+  end
+
+  def install
+    system "make", "lazy-extractors", "pypi-files" if build.head?
+    virtualenv_install_with_resources
+    bash_completion.install libexec/"share/bash-completion/completions/yt-dlp"
+    zsh_completion.install libexec/"share/zsh/site-functions/_yt-dlp"
+    fish_completion.install libexec/"share/fish/vendor_completions.d/yt-dlp.fish"
+  end
+
+  test do
+    system bin/"yt-dlp", "https://raw.githubusercontent.com/Homebrew/brew/refs/heads/master/Library/Homebrew/test/support/fixtures/test.gif"
+
+    system bin/"yt-dlp", "--simulate", "https://x.com/X/status/1922008207133671652"
+  end
+end
