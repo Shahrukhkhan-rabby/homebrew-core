91db9c55c6f31335d03efe93cc0c35665d8ce44d
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2026-08-08 14:59:57 +0600

    Formula w/wangle.rb

---
+++ Formula/w/wangle.rb
@@ -0,0 +1,86 @@
+class Wangle < Formula
+  desc "Modular, composable client/server abstractions framework"
+  homepage "https://github.com/facebook/wangle"
+  url "https://github.com/facebook/wangle/archive/refs/tags/v2026.05.18.00.tar.gz"
+  sha256 "9c6ce936a0306b022c328bef743d0c305e2ebacf825a1e023128bafed5c835a3"
+  license "Apache-2.0"
+  compatibility_version 1
+  head "https://github.com/facebook/wangle.git", branch: "main"
+
+  bottle do
+    sha256 cellar: :any,                 arm64_tahoe:   "790f1ff37b94c97fd9cefb6cf94d1a35e98935dbf1653be69f2b850835430660"
+    sha256 cellar: :any,                 arm64_sequoia: "c493b0893bdb8a7ad68eba0be031942d475c0b63a939c1a7233af035f1825378"
+    sha256 cellar: :any,                 arm64_sonoma:  "6be3839761c9477f2f98b9eaf17c655a27d8d3cf45ae0c9dcd095dc7cf97c938"
+    sha256 cellar: :any,                 sonoma:        "be792ea1f2c12f701ec6158d4590f24d021b10f2d658149fd4e4c2762aca55fc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e53b583deebe1324b59ce553a4885cc1131de5863c152864935e4d8e13044ddf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aef688d30638234d52cd2d954ab482bdabef503dd9a20258c58a2b196f56392c"
+  end
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "libevent" => :build
+  depends_on "double-conversion"
+  depends_on "fizz"
+  depends_on "fmt"
+  depends_on "folly"
+  depends_on "gflags"
+  depends_on "glog"
+  depends_on "openssl@3"
+
+  def install
+    args = ["-DBUILD_TESTS=OFF"]
+    # Prevent indirect linkage with boost, libsodium, snappy and xz
+    args << "-DCMAKE_SHARED_LINKER_FLAGS=-Wl,-dead_strip_dylibs" if OS.mac?
+
+    system "cmake", "-S", "wangle", "-B", "build/shared", "-DBUILD_SHARED_LIBS=ON", *args, *std_cmake_args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    system "cmake", "-S", "wangle", "-B", "build/static", "-DBUILD_SHARED_LIBS=OFF", *args, *std_cmake_args
+    system "cmake", "--build", "build/static"
+    lib.install "build/static/lib/libwangle.a"
+
+    pkgshare.install Dir["wangle/example/echo/*.cpp"]
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write <<~CMAKE
+      cmake_minimum_required(VERSION 4.0)
+      project(Echo LANGUAGES CXX)
+      set(CMAKE_CXX_STANDARD 20)
+
+      list(APPEND CMAKE_MODULE_PATH "#{Formula["fizz"].opt_libexec}/cmake")
+      find_package(gflags REQUIRED)
+      find_package(folly CONFIG REQUIRED)
+      find_package(fizz CONFIG REQUIRED)
+      find_package(wangle CONFIG REQUIRED)
+
+      add_executable(EchoClient #{pkgshare}/EchoClient.cpp)
+      target_link_libraries(EchoClient wangle::wangle)
+      add_executable(EchoServer #{pkgshare}/EchoServer.cpp)
+      target_link_libraries(EchoServer wangle::wangle)
+    CMAKE
+
+    ENV.delete "CPATH"
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_MODULE_PATH=#{testpath}/cmake", "-Wno-dev"
+    system "cmake", "--build", "build"
+
+    port = free_port
+    spawn testpath/"build/EchoServer", "-port", port.to_s
+    sleep 30
+
+    require "pty"
+    output = ""
+    PTY.spawn(testpath/"build/EchoClient", "-port", port.to_s) do |r, w, pid|
+      w.write "Hello from Homebrew!\nAnother test line.\n"
+      sleep 60
+      Process.kill "TERM", pid
+      begin
+        r.each_line { |line| output += line }
+      rescue Errno::EIO
+        # GNU/Linux raises EIO when read is done on closed pty
+      end
+    end
+    assert_match("Hello from Homebrew!", output)
+    assert_match("Another test line.", output)
+  end
+end
